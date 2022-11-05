971c921d8bbca197c34c0f5ce2069dff9f85d263
DonutLaser <vidmantas.luneckas@gmail.com>
2022-11-05 23:23:52 +0200

    .

--- main.go
+++ main.go
@@ -16,8 +16,10 @@ type Args struct {
 func printUsage() {
 	fmt.Println("Usage: schedule [subcommand]")
 	fmt.Println("Subcommands:")
-	fmt.Println("	edit		Edit the schedule")
-	fmt.Println("	cleanup		Remove schedule for days that have already passed")
+	fmt.Println("	edit				Edit the schedule")
+	fmt.Println("	tomorrow			Show schedule for tomorrow")
+	fmt.Println("	for <YYYY-MM-DD>	Show schedule for a specific date")
+	fmt.Println("	cleanup				Remove schedule for days that have already passed")
 }
 
 func parseArgs() (result Args, success bool) {
@@ -49,7 +51,7 @@ func main() {
 
 	if args.Subcommand == "edit" {
 		EditSchedule()
-	} else if args.Subcommand == "on" {
+	} else if args.Subcommand == "for" {
 		if args.Param == "" {
 			color.Red("Error: date is not specified\n\n")
 			printUsage()
@@ -59,6 +61,8 @@ func main() {
 		ShowSchedule(args.Param)
 	} else if args.Subcommand == "cleanup" {
 		CleanupSchedule()
+	} else if args.Subcommand == "tomorrow" {
+		ShowSchedule(time.Now().AddDate(0, 0, 1).Format("2006-01-02"))
 	} else {
 		ShowSchedule(time.Now().Format("2006-01-02"))
 	}
